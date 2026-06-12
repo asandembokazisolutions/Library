b05ff70d77720a05498a573dfd5628aed7cac861
Asande Mbokazi <asandeseaphesihle@gmail.com>
2026-06-12 13:29:39 +0200

    Refactor EnsurePopulated method to return Task
    Changed EnsurePopulated method to return a Task instead of void and removed unnecessary pending migrations check.

--- Library/Data/SeedIdentityData.cs
+++ Library/Data/SeedIdentityData.cs
@@ -1,4 +1,4 @@
-﻿using Library.Models;
+using Library.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,16 +12,13 @@ namespace Library.Data
         private const string adminRole = "Admin";
         private const string memberRole = "Member";
 
-        public static async void EnsurePopulated(IApplicationBuilder app)
+        public static async Task EnsurePopulated(IApplicationBuilder app)
         {
             AppIdentityDbContext context = app.ApplicationServices
                 .CreateScope().ServiceProvider
                 .GetRequiredService<AppIdentityDbContext>();
 
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            }
+            context.Database.Migrate();
 
             UserManager<AppUser> userManager = app.ApplicationServices
                 .CreateScope().ServiceProvider
@@ -31,14 +28,12 @@ namespace Library.Data
                 .CreateScope().ServiceProvider
                 .GetRequiredService<RoleManager<IdentityRole>>();
 
-            // Seed roles
             if (await roleManager.FindByNameAsync(adminRole) == null)
                 await roleManager.CreateAsync(new IdentityRole(adminRole));
 
             if (await roleManager.FindByNameAsync(memberRole) == null)
                 await roleManager.CreateAsync(new IdentityRole(memberRole));
 
-            // Seed admin user
             if (await userManager.FindByNameAsync(adminUserName) == null)
             {
                 AppUser admin = new AppUser
@@ -46,9 +41,7 @@ namespace Library.Data
                     UserName = adminUserName,
                     Email = adminEmail
                 };
-
                 IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
-
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(admin, adminRole);
